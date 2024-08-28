00139a274ab8cf7e32c85c9445afd3823c0effab
Damon Bayer <damon@damonbayer.com>
2024-08-28 15:33:55 -0500

    use DifferencedProcess directly

--- pyrenew_covid_wastewater/hosp_only_ww_model.py
+++ pyrenew_covid_wastewater/hosp_only_ww_model.py
@@ -1,10 +1,11 @@
 # numpydoc ignore=GL08
+import json
+
 import jax.numpy as jnp
 import numpyro
 import numpyro.distributions as dist
 import pyrenew.transformation as transformation
-from pyrenew.arrayutils import tile_until_n
-from pyrenew.deterministic import DeterministicVariable
+from pyrenew.arrayutils import repeat_until_n, tile_until_n
 from pyrenew.latent import (
     InfectionInitializationProcess,
     InfectionsWithFeedback,
@@ -12,7 +13,7 @@ from pyrenew.latent import (
 )
 from pyrenew.metaclass import Model
 from pyrenew.observation import NegativeBinomialObservation
-from pyrenew.process import ARProcess, RtWeeklyDiffARProcess
+from pyrenew.process import ARProcess, DifferencedProcess
 from pyrenew.randomvariable import DistributionalVariable
 
 
@@ -53,6 +54,14 @@ class hosp_only_ww_model(Model):  # numpydoc ignore=GL08
             infection_feedback_pmf=infection_feedback_pmf_rv,
         )
 
+        self.p_hosp_ar_proc = ARProcess("p_hosp")
+        self.ar_diff = DifferencedProcess(
+            fundamental_process=ARProcess(
+                noise_rv_name="rtu_weekly_diff_first_diff_ar_process_noise"
+            ),
+            differencing_order=1,
+        )
+
         self.autoreg_rt_rv = autoreg_rt_rv
         self.eta_sd_rv = eta_sd_rv
         self.log_r_mu_intercept_rv = log_r_mu_intercept_rv
@@ -91,65 +100,54 @@ class hosp_only_ww_model(Model):  # numpydoc ignore=GL08
         else:
             n_datapoints = n_datapoints
 
-        n_weeks = n_datapoints // 7 + 1
-
-        i0 = self.infection_initialization_process()
+        n_weeks_post_init = n_datapoints // 7 + 1
+        i0 = self.infection_initialization_process()[0].value
 
         eta_sd = self.eta_sd_rv()[0].value
-
         autoreg_rt = self.autoreg_rt_rv()[0].value
-        autoreg_rt_det_rv = DeterministicVariable("autoreg_rt_det", autoreg_rt)
-
         log_r_mu_intercept = self.log_r_mu_intercept_rv()[0].value
-
         init_rate_of_change_rv = DistributionalVariable(
             "init_rate_of_change",
             dist.Normal(0, eta_sd / jnp.sqrt(1 - jnp.pow(autoreg_rt, 2))),
         )
-
         init_rate_of_change = init_rate_of_change_rv()[0].value
 
-        rt_proc = RtWeeklyDiffARProcess(
-            name="rtu_weekly_diff",
+        log_rtu_weekly = self.ar_diff(
+            n=n_weeks_post_init,
+            init_vals=jnp.array(log_r_mu_intercept),
+            autoreg=jnp.array(autoreg_rt),
+            noise_sd=jnp.array(eta_sd),
+            fundamental_process_init_vals=jnp.array(init_rate_of_change),
+        )[0].value
+
+        rtu = repeat_until_n(
+            data=jnp.exp(log_rtu_weekly),
+            n_timepoints=n_datapoints,
             offset=0,
-            log_rt_rv=DeterministicVariable(
-                name="log_rt",
-                value=jnp.array(
-                    [
-                        log_r_mu_intercept,
-                        log_r_mu_intercept + init_rate_of_change,
-                    ]
-                ),
-            ),
-            autoreg_rv=autoreg_rt_det_rv,
-            periodic_diff_sd_rv=DeterministicVariable(
-                name="periodic_diff_sd", value=jnp.array(eta_sd)
-            ),
+            period_size=7,
         )
 
-        rtu = rt_proc.sample(duration=n_datapoints)
-        generation_interval_pmf = self.generation_interval_pmf_rv()
+        generation_interval_pmf = self.generation_interval_pmf_rv()[0].value
 
-        inf_with_feedback_proc_sample = self.inf_with_feedback_proc.sample(
-            Rt=rtu[0].value,
-            I0=i0[0].value,
-            gen_int=generation_interval_pmf[0].value,
+        inf_with_feedback_proc_sample = self.inf_with_feedback_proc(
+            Rt=rtu,
+            I0=i0,
+            gen_int=generation_interval_pmf,
         )
 
         latent_infections = jnp.concat(
             [
-                i0[0].value,
+                i0,
                 inf_with_feedback_proc_sample.post_initialization_infections.value,
             ]
         )
-        numpyro.deterministic("rtu", rtu[0].value)
+        numpyro.deterministic("rtu", rtu)
+        numpyro.deterministic("rt", inf_with_feedback_proc_sample.Rt.value)
         numpyro.deterministic("latent_infections", latent_infections)
 
         p_hosp_mean = self.p_hosp_mean_rv()[0].value
         p_hosp_w_sd = self.p_hosp_w_sd_rv()[0].value
         autoreg_p_hosp = self.autoreg_p_hosp_rv()[0].value
-
-        p_hosp_ar_proc = ARProcess("p_hosp")
 
         p_hosp_ar_init_rv = DistributionalVariable(
             "p_hosp_ar_init",
@@ -160,21 +158,19 @@ class hosp_only_ww_model(Model):  # numpydoc ignore=GL08
         )
         p_hosp_ar_init = p_hosp_ar_init_rv()[0].value
 
-        p_hosp_ar = p_hosp_ar_proc.sample(
-            n=n_weeks,
+        p_hosp_ar = self.p_hosp_ar_proc(
+            n=n_weeks_post_init,
             autoreg=autoreg_p_hosp,
             init_vals=p_hosp_ar_init,
             noise_sd=p_hosp_w_sd,
-        )
+        )[0].value
 
         ihr = jnp.repeat(
-            transformation.SigmoidTransform()(
-                p_hosp_ar[0].value + p_hosp_mean
-            ),
+            transformation.SigmoidTransform()(p_hosp_ar + p_hosp_mean),
             repeats=7,
         )[
             :n_datapoints
-        ]  # this is only applied after the hospital_admissions are generated, not to all the latent infectios
+        ]  # this is only applied after the hospital_admissions are generated, not to all the latent infections. This is why we cannot apply the ihr in compute_delay_ascertained_incidence
 
         numpyro.deterministic("ihr", ihr)
 
@@ -218,3 +214,12 @@ class hosp_only_ww_model(Model):  # numpydoc ignore=GL08
             latent_hospital_admissions,
             observed_hospital_admissions,
         )
+
+
+def fit_hosp_only_ww_model_from_stan_data(stan_data_file):
+    with open(
+        stan_data_file,
+        "r",
+    ) as file:
+        stan_data = json.load(file)
+    return stan_data
