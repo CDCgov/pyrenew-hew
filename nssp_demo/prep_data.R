f556ba89236d44849b1c17bb9410e9accefa796d
Damon Bayer <xum8@cdc.gov>
2024-09-14 00:13:48 +0000

    add generation_interval_pmf

--- nssp_demo/prep_data.R
+++ nssp_demo/prep_data.R
@@ -5,6 +5,7 @@ library(usa)
 library(fs)
 library(here)
 
+
 prep_data <- function(report_date = today(),
                       min_reference_date = "2000-01-01",
                       max_reference_date = "3000-01-01",
@@ -35,10 +36,18 @@ prep_data <- function(report_date = today(),
     filter(abb == state_abb) %>%
     pull(population)
 
+  generation_interval_pmf <-
+    read_csv(here(path("nssp_demo", "private_data", "latest", ext = "csv"))) %>%
+    filter(geo_value == str_to_lower(state_abb)) %>%
+    arrange(delay) %>%
+    pull(p)
+
+
   list(
     prepped_date = prepped_data,
     data_for_model_fit = list(
-      train_ed_admissions = train_ed_admissions,
+      generation_interval_pmf = generation_interval_pmf,
+      data_observed_hospital_admissions = train_ed_admissions,
       state_pop = state_pop
     )
   )
