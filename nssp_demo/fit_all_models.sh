7fa03f616cff65a98338cd9058a83e6c4bb7d153
Damon Bayer <xum8@cdc.gov>
2024-09-17 02:47:40 +0000

    revise fit all models script

--- nssp_demo/fit_all_models.sh
+++ nssp_demo/fit_all_models.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Base directory containing subdirectories
-BASE_DIR="/path/to/base/directory"
+BASE_DIR="private_data/r_2024-09-10_f_2024-03-13_l_2024-09-09_t_2024-08-14/"
 
 # Iterate over each subdirectory in the base directory
 for SUBDIR in "$BASE_DIR"/*/; do
     # Run the Python script with the current subdirectory as the model_dir argument
+    echo "$SUBDIR"
     python fit_model.py --model_dir "$SUBDIR"
 done
