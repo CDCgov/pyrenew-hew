179d4fd0b9edf4b244af494639ccba4d686e3762
Damon Bayer <xum8@cdc.gov>
2024-10-08 22:53:33 +0000

    split up fitting and predicting

--- nssp_demo/fit_model.py
+++ nssp_demo/fit_model.py
@@ -1,35 +1,13 @@
 import argparse
-import json
+import pickle
 from pathlib import Path
 
-import arviz as az
 import jax
-import jax.numpy as jnp
-import numpy as np
 import numpyro
-from pyrenew.deterministic import DeterministicVariable
-
-import pyrenew_hew.plotting as plotting
-from pyrenew_hew.hosp_only_ww_model import hosp_only_ww_model
 
 n_chains = 4
 numpyro.set_host_device_count(n_chains)
-
-# load priors
-# have to run this from the right directory
-from priors import (  # noqa: E402
-    autoreg_p_hosp_rv,
-    autoreg_rt_rv,
-    eta_sd_rv,
-    hosp_wday_effect_rv,
-    i0_first_obs_n_rv,
-    inf_feedback_strength_rv,
-    initialization_rate_rv,
-    log_r_mu_intercept_rv,
-    p_hosp_mean_rv,
-    p_hosp_w_sd_rv,
-    phi_rv,
-)
+from build_model import build_model_from_dir  # noqa: E402
 
 parser = argparse.ArgumentParser(
     description="Fit the hospital-only wastewater model."
@@ -41,69 +19,11 @@ parser.add_argument(
     help="Path to the model directory containing the data.",
 )
 args = parser.parse_args()
-
 model_dir = Path(args.model_dir)
-data_path = model_dir / "data_for_model_fit.json"
 
-with open(
-    data_path,
-    "r",
-) as file:
-    model_data = json.load(file)
-
-
-inf_to_hosp_rv = DeterministicVariable(
-    "inf_to_hosp", jnp.array(model_data["inf_to_hosp_pmf"])
-)  # check if off by 1 or reversed
-
-generation_interval_pmf_rv = DeterministicVariable(
-    "generation_interval_pmf", jnp.array(model_data["generation_interval_pmf"])
-)  # check if off by 1 or reversed
-
-infection_feedback_pmf_rv = DeterministicVariable(
-    "infection_feedback_pmf", jnp.array(model_data["generation_interval_pmf"])
-)  # check if off by 1 or reversed
-
-data_observed_hospital_admissions = jnp.array(
-    model_data["data_observed_hospital_admissions"]
+my_model, data_observed_hospital_admissions, right_truncation_offset = (
+    build_model_from_dir(model_dir)
 )
-state_pop = jnp.array(model_data["state_pop"])
-
-right_truncation_pmf_rv = DeterministicVariable(
-    "right_truncation_pmf", jnp.array(model_data["right_truncation_pmf"])
-)
-
-uot = (
-    max(
-        len(model_data["generation_interval_pmf"]),
-        len(model_data["inf_to_hosp_pmf"]),
-    )
-    - 1
-)
-
-right_truncation_offset = model_data["right_truncation_offset"]
-n_forecast_points = 28
-my_model = hosp_only_ww_model(
-    state_pop=state_pop,
-    i0_first_obs_n_rv=i0_first_obs_n_rv,
-    initialization_rate_rv=initialization_rate_rv,
-    log_r_mu_intercept_rv=log_r_mu_intercept_rv,
-    autoreg_rt_rv=autoreg_rt_rv,
-    eta_sd_rv=eta_sd_rv,  # sd of random walk for ar process,
-    generation_interval_pmf_rv=generation_interval_pmf_rv,
-    infection_feedback_strength_rv=inf_feedback_strength_rv,
-    infection_feedback_pmf_rv=infection_feedback_pmf_rv,
-    p_hosp_mean_rv=p_hosp_mean_rv,
-    p_hosp_w_sd_rv=p_hosp_w_sd_rv,
-    autoreg_p_hosp_rv=autoreg_p_hosp_rv,
-    hosp_wday_effect_rv=hosp_wday_effect_rv,
-    inf_to_hosp_rv=inf_to_hosp_rv,
-    phi_rv=phi_rv,
-    right_truncation_pmf_rv=right_truncation_pmf_rv,
-    n_initialization_points=uot,
-)
-
-
 my_model.run(
     num_warmup=500,
     num_samples=500,
@@ -114,33 +34,10 @@ my_model.run(
     nuts_args=dict(find_heuristic_step_size=True),
 )
 
+my_model.mcmc.sampler = None
 
-posterior_predictive = my_model.posterior_predictive(
-    n_datapoints=len(data_observed_hospital_admissions) + n_forecast_points
-)
-
-idata = az.from_numpyro(
-    my_model.mcmc,
-    posterior_predictive=posterior_predictive,
-)
-
-chain_ll = (
-    idata["log_likelihood"]
-    .mean(dim=["observed_hospital_admissions_dim_0", "draw"])[
-        "observed_hospital_admissions"
-    ]
-    .values
-)
-
-chains_to_keep = np.arange(n_chains)[
-    ((chain_ll - chain_ll.max()) / chain_ll.max()) < 2
-]
-# would like to not have to run this
-
-idata = idata.sel(chain=chains_to_keep)
-
-plotting.plot_predictive(idata)
-
-idata.to_dataframe().to_csv(
-    model_dir / "pyrenew_inference_data.csv", index=False
-)
+with open(
+    model_dir / "pyrenew_model_posterior.pickle",
+    "wb",
+) as file:
+    pickle.dump(my_model.mcmc, file)
