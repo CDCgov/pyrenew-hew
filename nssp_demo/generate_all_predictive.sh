b83dbdab3f832a3faa4cc68fff4faba92c568f5e
Damon Bayer <xum8@cdc.gov>
2024-10-10 16:14:49 +0000

    script to generate all predictive files

---
+++ nssp_demo/generate_all_predictive.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Base directory containing subdirectories
+
+BASE_DIR="private_data/influenza_r_2024-10-01_f_2024-04-03_l_2024-09-30_t_2024-09-25"
+
+
+# Iterate over each subdirectory in the base directory
+for SUBDIR in "$BASE_DIR"/*/; do
+    # Run the Python script with the current subdirectory as the model_dir argument
+    echo "$SUBDIR"
+    python generate_predictive.py --model_dir "$SUBDIR"
+done
