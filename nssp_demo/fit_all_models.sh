9803c59778702485dc67965a755915fc9dae8816
Damon Bayer <damon@damonbayer.com>
2024-10-10 12:17:42 -0500

    make base_dir a command line argument in fit_all_models and generate_all_predictive

--- nssp_demo/fit_all_models.sh
+++ nssp_demo/fit_all_models.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Check if the base directory is provided as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <base_dir>"
+    exit 1
+fi
+
 # Base directory containing subdirectories
-
-BASE_DIR="private_data/influenza_r_2024-10-01_f_2024-04-03_l_2024-09-30_t_2024-09-25"
-
+BASE_DIR="$1"
 
 # Iterate over each subdirectory in the base directory
 for SUBDIR in "$BASE_DIR"/*/; do
