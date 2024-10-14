ffcbdfddfe2770da8fe522568d9fb6bd5d31e525
Samuel Brand <sam055@mac.com>
2024-10-14 15:13:04 +0100

    reformat tests

--- countpredr/tests/testthat/generate_example_data.R
+++ countpredr/tests/testthat/generate_example_data.R
@@ -6,11 +6,11 @@ test_that("generate_example_data saves the CSV file with correct data", {
     temp_dir <- tempdir()
     output_dir <- file.path(temp_dir, "test_output")
     output_file <- file.path(output_dir, "exampledata_covid.csv")
-    
+
     generate_example_data(output_dir = output_dir)
-    
+
     expect_true(file.exists(output_file))
-    
+
     exampledata <- read.csv(output_file)
     expect_true("date" %in% names(exampledata))
     expect_true("target_resp_est" %in% names(exampledata))
@@ -21,9 +21,9 @@ test_that("generate_example_data doesnt the CSV file when savedata = FALSE", {
     temp_dir <- tempdir()
     output_dir <- file.path(temp_dir, "test_output")
     output_file <- file.path(output_dir, "exampledata_rsv.csv")
-    
+
     generate_example_data(target = "rsv", output_dir = output_dir, savedata = FALSE)
-    
+
     expect_false(file.exists(output_file))
 })
 
@@ -31,12 +31,12 @@ test_that("generate_example_data has non-negative values and above test non resp
     temp_dir <- tempdir()
     output_dir <- file.path(temp_dir, "test_output")
     output_file <- file.path(output_dir, "exampledata_covid.csv")
-    
+
     test_est_non_resp <- 2e6
     generate_example_data(est_non_resp = test_est_non_resp, output_dir = output_dir)
-    
+
     exampledata <- read.csv(output_file)
-    
+
     # Check that the estimates are within expected ranges
     expect_true(all(exampledata$target_resp_est >= 0))
     expect_true(all(exampledata$other_ed_visits >= test_est_non_resp))
