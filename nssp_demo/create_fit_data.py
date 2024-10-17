ff55602b4013ec0e966345802ec8f8e007cfe7f9
Damon Bayer <xum8@cdc.gov>
2024-10-17 19:46:13 +0000

    add nnh_estimates

--- nssp_demo/create_fit_data.py
+++ nssp_demo/create_fit_data.py
@@ -3,6 +3,7 @@ import pathlib
 from datetime import timedelta
 
 import duckdb
+import polars as pl
 import pyarrow.parquet as pq
 
 disease_map = {
@@ -14,9 +15,36 @@ disease = "COVID-19"
 # todo read these from cli
 
 # todo also read dates from cli
+from datetime import datetime
 
-data_path = pathlib.Path("private_data/2024-10-10.parquet")
-nssp_data = duckdb.arrow(pq.read_table(data_path))
+reference_date = datetime.strptime("2024-10-10", "%Y-%m-%d").date()
+nssp_data = duckdb.arrow(
+    pq.read_table(f"private_data/{reference_date}.parquet")
+)
+nnh_estimates = pl.from_arrow(pq.read_table("private_data/prod.parquet"))
+
+
+generation_interval_pmf = (
+    nnh_estimates.filter(
+        (pl.col("geo_value").is_null())
+        & (pl.col("disease") == disease)
+        & (pl.col("parameter") == "generation_interval")
+        & (pl.col("end_date").is_null())
+    )
+    .get_column("value")
+    .to_list()[0]
+)
+
+delay_pmf = (
+    nnh_estimates.filter(
+        (pl.col("geo_value").is_null())
+        & (pl.col("disease") == disease)
+        & (pl.col("parameter") == "delay")
+        & (pl.col("end_date").is_null())
+    )
+    .get_column("value")
+    .to_list()[0]
+)
 
 all_geos = (
     nssp_data.unique("geo_value")
@@ -43,6 +71,19 @@ for state_abb in all_geos:
     actual_first_date = data_to_save_pl["reference_date"].min()
     actual_last_date = data_to_save_pl["reference_date"].max()
     last_training_date = actual_last_date - timedelta(days=7)
+
+    right_truncation_pmf = (
+        nnh_estimates.filter(
+            (pl.col("geo_value") == state_abb)
+            & (pl.col("disease") == disease)
+            & (pl.col("parameter") == "right_truncation")
+            & (pl.col("end_date").is_null())
+            & (pl.col("reference_date") <= reference_date)
+        )
+        .filter(pl.col("reference_date") == pl.col("reference_date").max())
+        .get_column("value")
+        .to_list()[0]
+    )
 
     model_folder_name = f"{disease.lower()}_r_{report_date}_f_{actual_first_date}_l_{actual_last_date}_t_{last_training_date}"
 
