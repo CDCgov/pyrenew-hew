93a479ebd2dbdb82c6af2dc8abe672af9698d340
Damon Bayer <xum8@cdc.gov>
2024-10-17 23:01:05 +0000

    reproduce all r functionality

--- nssp_demo/create_fit_data.py
+++ nssp_demo/create_fit_data.py
@@ -15,9 +15,17 @@ disease = "COVID-19"
 # todo read these from cli
 
 # todo also read dates from cli
+import json
 from datetime import datetime
 
 reference_date = datetime.strptime("2024-10-10", "%Y-%m-%d").date()
+training_day_offset = 7
+n_training_days = 1
+
+last_training_date = reference_date - timedelta(days=training_day_offset + 1)
+# +1 because max date in dataset is reference_date - 1
+first_training_date = last_training_date - timedelta(days=n_training_days - 1)
+
 nssp_data = duckdb.arrow(
     pq.read_table(f"private_data/{reference_date}.parquet")
 )
@@ -46,20 +54,36 @@ delay_pmf = (
     .to_list()[0]
 )
 
-all_geos = (
+all_states = (
     nssp_data.unique("geo_value")
     .order("geo_value")
     .pl()["geo_value"]
     .to_list()
 )
 
-for state_abb in all_geos:
+
+facts = pl.read_csv(
+    "https://raw.githubusercontent.com/k5cents/usa/refs/heads/master/data-raw/facts.csv"
+)
+states = pl.read_csv(
+    "https://raw.githubusercontent.com/k5cents/usa/refs/heads/master/data-raw/states.csv"
+)
+
+state_pop_df = facts.join(states, on="name").select(
+    ["abb", "name", "population"]
+)
+
+for state_abb in all_states:
     data_to_save = duckdb.sql(
         f"""
-        SELECT report_date, reference_date, SUM(value) AS value
+        SELECT report_date, reference_date, SUM(value) AS ED_admissions,
+        CASE WHEN reference_date <= '{first_training_date}'
+            THEN 'train'
+            ELSE 'test' END AS data_type
         FROM nssp_data
         WHERE disease = '{disease_map[disease]}' AND metric = 'count_ed_visits'
         AND geo_value = '{state_abb}'
+        and reference_date >= '{first_training_date}'
         GROUP BY report_date, reference_date
         ORDER BY report_date, reference_date
         """
@@ -67,10 +91,16 @@ for state_abb in all_geos:
     # why not count_admitted_ed_visits ?
 
     data_to_save_pl = data_to_save.pl()
+
     report_date = data_to_save_pl[0, "report_date"]
     actual_first_date = data_to_save_pl["reference_date"].min()
     actual_last_date = data_to_save_pl["reference_date"].max()
-    last_training_date = actual_last_date - timedelta(days=7)
+
+    state_pop = (
+        state_pop_df.filter(pl.col("abb") == state_abb)
+        .get_column("population")
+        .to_list()[0]
+    )
 
     right_truncation_pmf = (
         nnh_estimates.filter(
@@ -85,12 +115,36 @@ for state_abb in all_geos:
         .to_list()[0]
     )
 
+    train_ed_admissions = (
+        data_to_save_pl.filter(pl.col("data_type") == "train")
+        .get_column("ED_admissions")
+        .to_list()
+    )
+    test_ed_admissions = (
+        data_to_save_pl.filter(pl.col("data_type") == "test")
+        .get_column("ED_admissions")
+        .to_list()
+    )
+
+    data_for_model_fit = {
+        "inf_to_hosp_pmf": delay_pmf,
+        "generation_interval_pmf": generation_interval_pmf,
+        "right_truncation_pmf": right_truncation_pmf,
+        "data_observed_hospital_admissions": train_ed_admissions,
+        "test_ed_admissions": test_ed_admissions,
+        "state_pop": state_pop,
+    }
+
     model_folder_name = f"{disease.lower()}_r_{report_date}_f_{actual_first_date}_l_{actual_last_date}_t_{last_training_date}"
 
     model_folder = pathlib.Path("private_data", model_folder_name)
     os.makedirs(model_folder, exist_ok=True)
-    data_folder = pathlib.Path(model_folder, state_abb)
-    os.makedirs(data_folder, exist_ok=True)
+    state_folder = pathlib.Path(model_folder, state_abb)
+    os.makedirs(state_folder, exist_ok=True)
     print(f"Saving {state_abb}")
-    data_to_save.to_csv(str(pathlib.Path(data_folder, "data.csv")))
-    # todo: save as tsv
+    data_to_save.to_csv(str(pathlib.Path(state_folder, "data.csv")))
+
+    with open(
+        pathlib.Path(state_folder, "data_for_model_fit.json"), "w"
+    ) as json_file:
+        json.dump(data_for_model_fit, json_file)
