a21a0acb14ca3f774c8e6913048153b05a0bb270
Samuel Brand <sam055@mac.com>
2024-10-25 23:53:46 +0100

    vector -> string

--- scoringutilhelpers/R/join_forecast_and_data.R
+++ scoringutilhelpers/R/join_forecast_and_data.R
@@ -7,7 +7,7 @@ library(dplyr)
 #' This function reads forecast data from a Parquet file and actual data from a
 #' TSV file, then joins them using a specified key.
 #'
-#' @param forecast_source A character vector specifying the path to the
+#' @param forecast_source A character string specifying the path to the
 #' directory containing Parquet file(s) containing forecast data.
 #' @param data_path A character string specifying the path to the TSV file
 #' containing actual/truth data.
