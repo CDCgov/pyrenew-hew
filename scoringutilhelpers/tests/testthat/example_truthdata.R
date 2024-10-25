16d15736acb081dc8cf83d3ec22168a32fe6e72b
Samuel Brand <sam055@mac.com>
2024-10-26 00:15:46 +0100

    de snake ify and add expand

--- scoringutilhelpers/tests/testthat/example_truthdata.R
+++ scoringutilhelpers/tests/testthat/example_truthdata.R
@@ -1,20 +1,20 @@
-test_that("exampletruthdata generates correct number of rows and columns", {
+test_that("example_truthdata generates correct number of rows and columns", {
   ndays <- 10
   nareas <- 2
-  result <- exampletruthdata(ndays = ndays, nareas = nareas)
+  result <- example_truthdata(ndays = ndays, nareas = nareas)
   expect_equal(nrow(result), ndays * nareas)
   expect_equal(ncol(result), 3)
 })
 
-test_that("exampletruthdata generates correct column names", {
-  result <- exampletruthdata()
+test_that("example_truthdata generates correct column names", {
+  result <- example_truthdata()
   expect_true(all(c("area", "date", "truthdata") %in% colnames(result)))
 })
 
-test_that("exampletruthdata generates correct date range", {
+test_that("example_truthdata generates correct date range", {
   ndays <- 17
   start_date <- ymd("2024-10-24")
-  result <- exampletruthdata(ndays = ndays)
+  result <- example_truthdata(ndays = ndays)
   expected_dates <- seq.Date(
     from = start_date, by = "day",
     length.out = ndays
@@ -22,16 +22,16 @@ test_that("exampletruthdata generates correct date range", {
   expect_equal(unique(result$date), expected_dates)
 })
 
-test_that("exampletruthdata generates correct number of unique areas", {
+test_that("example_truthdata generates correct number of unique areas", {
   nareas <- 5
-  result <- exampletruthdata(nareas = nareas)
+  result <- example_truthdata(nareas = nareas)
   expect_equal(length(unique(result$area)), nareas)
 })
 
-test_that("exampletruthdata saves data when save_data is TRUE", {
+test_that("example_truthdata saves data when save_data is TRUE", {
   save_path <- tempdir()
-  filename <- file.path(save_path, "exampletruthdata.tsv")
-  result <- exampletruthdata(save_path = save_path, save_data = TRUE)
+  filename <- file.path(save_path, "example_truthdata.tsv")
+  result <- example_truthdata(save_path = save_path, save_data = TRUE)
   expect_true(file.exists(filename))
   saved_data <- readr::read_tsv(filename)
   expect_equal(nrow(saved_data), nrow(result))
