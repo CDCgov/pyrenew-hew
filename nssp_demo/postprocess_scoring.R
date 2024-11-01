9cc3609fb17f385fb69e360a534562f95f7d62fc
Dylan H. Morris <dylan@dylanhmorris.com>
2024-11-01 15:03:24 -0400

    Some more plots

--- nssp_demo/postprocess_scoring.R
+++ nssp_demo/postprocess_scoring.R
@@ -51,39 +51,129 @@ summarised_scoring_table <- function(score_table, scale = "natural") {
     group_by(model) |>
     summarise(rel_wis = mean(wis_scaled_relative_skill))
 
-    abs_wis <- score_table$quantile_scores |>
-        filter(scale == !!scale) |>
-        summarise_scores(by = "model") |>
-        select(model, abs_wis = wis, mae = ae_median, interval_coverage_50,
-            interval_coverage_90)
+  abs_wis <- score_table$quantile_scores |>
+    filter(scale == !!scale) |>
+    summarise_scores(by = "model") |>
+    select(model,
+      abs_wis = wis, mae = ae_median, interval_coverage_50,
+      interval_coverage_90
+    )
 
-    summarised_scores <- left_join(abs_wis, rel_wis, by = "model")
-    return(summarised_scores)
+  summarised_scores <- left_join(abs_wis, rel_wis, by = "model")
+  return(summarised_scores)
 }
 
 #' @title Epiweekly Scoring Plot
 #' This function generates a line plot of the weighted interval scores (WIS) for
 #' different models over epiweeks.
-#' @param score_table A scoring object containing the scoring table with
-#' quantile scores.
-#' @param scale A character string specifying the scale to filter the quantile
+#' @param quantile_scores A scoringutils output table
+#' containing the unsummarised quantile scores.
+#' @param scale A character string specifying the scale
+#' to filter the quantile
 #' scores. Default is "natural".
 #' @return A ggplot object representing the epiweekly scoring plot.
-epiweekly_scoring_plot <- function(score_table, scale = "natural") {
-   epiweekly_score_fig <- score_table$quantile_scores |>
-        filter(scale == !!scale) |>
-        mutate(epiweek = epiweek(date), epiyear = epiyear(date)) |>
-        get_pairwise_comparisons(by = c("epiweek", "epiyear"),
-            baseline = "cdc_baseline") |>
-        mutate(epidate = epiweek_to_date(epiweek, epiyear)) |>
-        group_by(model, epidate) |>
-        summarise(wis = mean(wis_scaled_relative_skill)) |>
-        as_tibble() |>
-        ggplot(aes(x = epidate, y = wis, color = model)) +
-        geom_line() +
-        geom_point() +  # Add points to the line plot
-        labs(title = "Epiweekly Scoring by Model",
-             x = "Epiweek start dates",
-             y = "Relative Weighted Interval Score (WIS)") +
-        theme_minimal()
+epiweekly_scoring_plot <- function(quantile_scores, scale = "natural") {
+  epiweekly_score_fig <- quantile_scores |>
+    filter(scale == !!scale) |>
+    mutate(epiweek = epiweek(date), epiyear = epiyear(date)) |>
+    get_pairwise_comparisons(
+      by = c("epiweek", "epiyear"),
+      baseline = "cdc_baseline"
+    ) |>
+    mutate(epidate = epiweek_to_date(epiweek, epiyear)) |>
+    group_by(model, epidate) |>
+    summarise(
+      wis = mean(wis_scaled_relative_skill),
+      .groups = "drop"
+    ) |>
+    as_tibble() |>
+    ggplot(aes(x = epidate, y = wis, color = model)) +
+    geom_line() +
+    geom_point() + # Add points to the line plot
+    labs(
+      title = "Epiweekly Scoring by Model",
+      x = "Epiweek start dates",
+      y = "Relative Weighted Interval Score (WIS)"
+    ) +
+    theme_minimal()
+
+  return(epiweekly_score_fig)
+}
+
+location_rel_wis_plot <- function(location, quantile_scores, ...) {
+  return(epiweekly_scoring_plot(
+    quantile_scores |>
+      dplyr::filter(location == !!location),
+    ...
+  ) + ggtitle(
+    glue::glue("Relative WIS over time for {location}")
+  ))
+}
+
+#' Save a list of plots as a PDF, with a
+#' grid of `nrow` by `ncol` plots per page
+#'
+#' @param list_of_plots list of plots to save to PDF
+#' @param save_path path to which to save the plots
+#' @param nrow Number of rows of plots per page
+#' (passed to [gridExtra::marrangeGrob()])
+#' Default `1`.
+#' @param ncol Number of columns of plots per page
+#' (passed to [gridExtra::marrangeGrob()]).
+#' Default `1`.
+#' @param width page width in device units (passed to
+#' [ggplot2::ggsave()]). Default `8.5`.
+#' @param height page height in device units (passed to
+#' [ggplot2::ggsave()]). Default `11`.
+#' @return `TRUE` on success.
+#' @export
+plots_to_pdf <- function(list_of_plots,
+                         save_path,
+                         nrow = 1,
+                         ncol = 1,
+                         width = 8.5,
+                         height = 11) {
+  if (!stringr::str_ends(
+    save_path, ".pdf"
+  )) {
+    cli::cli_abort("Filepath must end with `.pdf`")
+  }
+  cli::cli_inform("Saving plots to {save_path}")
+  ggplot2::ggsave(
+    filename = save_path,
+    plot = gridExtra::marrangeGrob(list_of_plots,
+      nrow = nrow,
+      ncol = ncol
+    ),
+    width = width,
+    height = height
+  )
+  return(TRUE)
+}
+
+
+main <- function(path_to_scores) {
+  scores <- readRDS(path_to_scores)
+
+  quantile_scores <- scores$quantile_scores
+  locations <- unique(quantile_scores$location) |>
+    purrr::set_names()
+  overall_fig <- epiweekly_scoring_plot(
+    quantile_scores,
+    scale = "log"
+  )
+
+  location_figs <- purrr::map(locations,
+    location_rel_wis_plot,
+    quantile_scores = quantile_scores,
+    scale = "log"
+  )
+
+  ggsave("ws_by_time_overall.pdf", overall_fig)
+
+  plots_to_pdf(location_figs,
+    "wis_by_time_and_location.pdf",
+    width = 7,
+    height = 5
+  )
 }
