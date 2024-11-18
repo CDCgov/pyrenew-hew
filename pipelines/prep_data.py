b67432fc82542873187d46bf4e6e3b21814469b9
Dylan H. Morris <dylan@dylanhmorris.com>
2024-11-18 11:02:50 -0500

    Concretize at aggregate to national step

--- pipelines/prep_data.py
+++ pipelines/prep_data.py
@@ -19,7 +19,7 @@ def aggregate_to_national(
     geo_values_to_include,
     first_date_to_include: datetime.date,
     national_geo_value="US",
-):
+) -> pl.DataFrame:
     assert national_geo_value not in geo_values_to_include
     return (
         data.filter(
@@ -28,6 +28,7 @@ def aggregate_to_national(
         )
         .group_by(["disease", "metric", "geo_type", "reference_date"])
         .agg(geo_value=pl.lit(national_geo_value), value=pl.col("value").sum())
+        .collect()
     )
 
 
