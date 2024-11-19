39490626c06fe81c4b196ec1883aeede4fb73d86
Dylan H. Morris <dylan@dylanhmorris.com>
2024-11-19 14:16:44 -0500

    Add tests for model_run_dir_path as well

--- hewr/tests/testthat/test_directory_utils.R
+++ hewr/tests/testthat/test_directory_utils.R
@@ -1,26 +1,53 @@
-test_that("parse_model_batch_dir_path() works as expected.", {
-  valid_pairs <- list(
-    list(
-      dirname = "covid-19_r_2024-02-03_f_2021-04-01_t_2024-01-23",
-      expected = list(
-        disease = "COVID-19",
-        report_date = lubridate::ymd("2024-02-03"),
-        first_training_date = lubridate::ymd("2021-04-1"),
-        last_training_date = lubridate::ymd("2024-01-23")
-      )
-    ),
-    list(
-      dirname = "influenza_r_2022-12-11_f_2021-02-05_t_2027-12-30",
-      expected = list(
-        disease = "Influenza",
-        report_date = lubridate::ymd("2022-12-11"),
-        first_training_date = lubridate::ymd("2021-02-5"),
-        last_training_date = lubridate::ymd("2027-12-30")
-      )
+valid_model_batch_dirs <- list(
+  list(
+    dirname = "covid-19_r_2024-02-03_f_2021-04-01_t_2024-01-23",
+    expected = list(
+      disease = "COVID-19",
+      report_date = lubridate::ymd("2024-02-03"),
+      first_training_date = lubridate::ymd("2021-04-1"),
+      last_training_date = lubridate::ymd("2024-01-23")
+    )
+  ),
+  list(
+    dirname = "influenza_r_2022-12-11_f_2021-02-05_t_2027-12-30",
+    expected = list(
+      disease = "Influenza",
+      report_date = lubridate::ymd("2022-12-11"),
+      first_training_date = lubridate::ymd("2021-02-5"),
+      last_training_date = lubridate::ymd("2027-12-30")
     )
   )
+)
 
-  for (valid_pair in valid_pairs) {
+invalid_model_batch_dirs <- c(
+  "qcovid-19_r_2024-02-03_f_2021-04-01_t_2024-01-23",
+  "influenza_r_2022-12-33_f_2021-02-05_t_2027-12-30"
+)
+
+to_valid_run_dir <- function(valid_batch_dir_entry, location) {
+  x <- valid_batch_dir_entry
+  x$dirpath <- fs::path(x$dirname, location)
+  x$expected <- c(
+    location = location,
+    x$expected
+  )
+  return(x)
+}
+
+valid_model_run_dirs <- c(
+  lapply(
+    valid_model_batch_dirs, to_valid_run_dir,
+    location = "ME"
+  ),
+  lapply(
+    valid_model_batch_dirs, to_valid_run_dir,
+    location = "US"
+  )
+)
+
+
+test_that("parse_model_batch_dir_path() works as expected.", {
+  for (valid_pair in valid_model_batch_dirs) {
     ## should work with base dirnames that are valid
     expect_equal(
       parse_model_batch_dir_path(valid_pair$dirname),
@@ -29,7 +56,7 @@ test_that("parse_model_batch_dir_path() works as expected.", {
 
     ## should work identically with a full path rather
     ## than just base dir
-    also_valid <- fs::path("this/is/a/test", valid_pair$dirname)
+    also_valid <- fs::path("this", "is", "a", "test", valid_pair$dirname)
     expect_equal(
       parse_model_batch_dir_path(also_valid),
       valid_pair$expected
@@ -47,17 +74,39 @@ test_that("parse_model_batch_dir_path() works as expected.", {
   }
 
   ## should error if entries cannot be parsed as what is expected
-  invalid_entries <- c(
-    "qcovid-19_r_2024-02-03_f_2021-04-01_t_2024-01-23",
-    "influenza_r_2022-12-33_f_2021-02-05_t_2027-12-30"
-  )
 
-  for (invalid_entry in invalid_entries) {
+  for (invalid_entry in invalid_model_batch_dirs) {
     expect_error(
       {
         parse_model_batch_dir_path(invalid_entry)
       },
       regex = "Could not parse extracted disease and/or date values"
+    )
+  }
+})
+
+test_that("parse_model_run_dir_path() works as expected.", {
+  for (valid_pair in valid_model_run_dirs) {
+    expect_equal(
+      parse_model_run_dir_path(valid_pair$dirpath),
+      valid_pair$expected
+    )
+
+    ## should work identically with a longer path
+    expect_equal(
+      parse_model_run_dir_path(fs::path(
+        "this", "is", "a", "test",
+        valid_pair$dirpath
+      )),
+      valid_pair$expected
+    )
+
+    ## should fail if there is additional terminal pathing
+    expect_error(
+      {
+        parse_model_run_dir_path(fs::path(valid_pair$dirpath, "test"))
+      },
+      regex = "Invalid format for model batch directory name"
     )
   }
 })
