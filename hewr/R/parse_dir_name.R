21f4218e8801a806fc86da24895b379ba4544fbb
Dylan H. Morris <dylan@dylanhmorris.com>
2024-11-18 21:09:44 -0500

    more accurate function name

--- hewr/R/parse_dir_name.R
+++ hewr/R/parse_dir_name.R
@@ -51,13 +51,13 @@ parse_model_batch_dir_name <- function(model_batch_dir_name) {
 #' `first_training_date`, and `last_training_date`.
 #'
 #' @export
-parse_model_run_dir_name <- function(model_run_dir_path) {
+parse_model_run_dir_path <- function(model_run_dir_path) {
   batch_dir <- fs::path_dir(model_run_dir_path) |>
     fs::path_file()
   location <- fs::path_file(model_run_dir_path)
 
   return(c(
     list(location = location),
-    parse_model_batch_dir_name(batch_dir)
+    parse_model_batch_dir(batch_dir)
   ))
 }
