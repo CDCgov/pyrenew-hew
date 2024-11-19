732b9a2a82b9b4ac54480ee4de8ecc8a48006c68
Samuel Brand <sam055@mac.com>
2024-11-19 13:55:37 +0000

    Update loop_postprocess.sh

--- pipelines/iteration_helpers/loop_postprocess.sh
+++ pipelines/iteration_helpers/loop_postprocess.sh
@@ -13,5 +13,5 @@ BASE_DIR="$1"
 for SUBDIR in "$BASE_DIR"/*/; do
     # Run the R script with the current subdirectory as the model_dir argument
     echo "$SUBDIR"
-    Rscript postprocess_state_forecast.R --model-run-dir "$SUBDIR"
+    Rscript postprocess_state_forecast.R "$SUBDIR"
 done
