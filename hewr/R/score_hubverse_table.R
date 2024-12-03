964173226b2a059d9683888605a45f2d0f0b5093
Dylan H. Morris <dylan@dylanhmorris.com>
2024-12-03 16:25:36 -0500

    Modularize hubverse scoring, use purrr throughout

---
+++ hewr/R/score_hubverse_table.R
@@ -0,0 +1,86 @@
+#' Read in and score hubverse-format output from `pyrenew-hew`.
+#'
+#' Expects forecast output created by [to_epiweekly_quantile_table()]
+#' and an observed data table with location, date, and value columns.
+#' The column names in the observed data table can be configured;
+#' defaults are `"location"`, `"reference_date"`, and
+#' `"value"`, respectively, to reflecting the format used
+#' in pyrenew-hew pipeline scripts such as
+#' `pipelines/create_observed_data_table.py`.
+#'
+#' @param forecast forecasts, as a hubverse-format
+#' [`tibble`][tibble::tibble()] produced by
+#' [to_epiweekly_quantile_table()], with columns
+#' `target_end_date`, `value`, and `horizon`
+#' @param observed observations, as a [`tibble`][tibble::tibble()].
+#' @param horizons Horizons to score. Default `c(0, 1)`
+#' @param transform transformation passed as the
+#' `fun` argument to [scoringutils::transform_forecasts()].
+#' Default [scoringutils::log_shift()].
+#' @param offset Offset for the transform passed to
+#' [scoringutils::transform_forcasts()]
+#' transforming forecasts for scoring. Default 1.
+#' @param observed_value_column Name of the column containing
+#' observed values in the `observed` table, as a string.
+#' Default `"value"`
+#' @param observed_location_column Name of the column containing
+#' location values in the `observed` table, as a string.
+#' Default `"location"`
+#' @param observed_date_column Name of the column containing
+#' date values in the `observed` table, as a string.
+#' Default `"reference_date"`
+#' @param ... Other keyword arguments passed to
+#' [scoringutils::tranform_forecasts()].
+#' @return A table of scores, as the output of
+#' [scoringutils::score()], filtered to include only the
+#' transformed_scale.
+#' @export
+score_hubverse <- function(forecast,
+                           observed,
+                           horizons = c(0, 1),
+                           transform = scoringutils::log_shift,
+                           offset = 1,
+                           observed_value_column = "value",
+                           observed_location_column = "location",
+                           observed_date_column = "reference_date") {
+  obs <- observed |>
+    dplyr::select(
+      location = .data[[observed_location_column]],
+      target_end_date = .data[[oberved_date_column]],
+      observed = .data[[observed_value_column]]
+    )
+
+  to_score <- forecast |>
+    dplyr::filter(.data$horizon %in% !!horizons) |>
+    dplyr::inner_join(obs,
+      by = c(
+        "location",
+        "target_end_date"
+      )
+    ) |>
+    scoringutils::as_forecast_quantile(
+      predicted = "value",
+      observed = "observed",
+      quantile_level = "output_type_id"
+    ) |>
+    scoringutils::transform_forecasts(
+      fun = transform,
+      offset = offset
+    )
+
+  interval_coverage_95 <- purrr::partial(
+    scoringutils::interval_coverage,
+    interval_range = 95
+  )
+
+  scored <- to_score |>
+    scoringutils::score(
+      metrics = c(
+        scoringutils::get_metrics(to_score),
+        list(interval_coverage_95 = interval_coverage_95)
+      )
+    ) |>
+    dplyr::filter(.data$scale == "log")
+
+  return(scored)
+}
--- pipelines/score_hubverse.R
+++ pipelines/score_hubverse.R
@@ -1,118 +1,125 @@
-library(ggplot2)
-library(hewr)
+library(argparser)
 
-score_hubverse <- function(path_observed,
-                           path_forecast,
-                           observed_column,
-                           horizons = c(0, 1),
-                           log_shift_offset = 1) {
-  obs <- readr::read_tsv(path_observed,
-    show_col_types = FALSE
-  )
-  forecast <- readr::read_tsv(path_forecast,
-    show_col_types = FALSE
-  )
-
-  interval_coverage_95 <- purrr::partial(
-    scoringutils::interval_coverage,
-    interval_range = 95
-  )
-
-  to_score <- dplyr::inner_join(
-    forecast |> dplyr::filter(horizon %in% !!horizons),
-    obs |> dplyr::select(
-      location,
-      target_end_date = reference_date,
-      observed = {{ observed_column }}
+get_hubverse_table_paths <- function(dir,
+                                     disease) {
+  path_df <- tibble::tibble(
+    path = fs::dir_ls(
+      dir,
+      type = "file",
+      glob = "*-{disease}-hubverse-table.tsv"
     ),
-    by = c("location", "target_end_date")
-  ) |>
-    scoringutils::as_forecast_quantile(
-      predicted = "value",
-      observed = "observed",
-      quantile_level = "output_type_id"
-    ) |>
-    scoringutils::transform_forecasts(
-      fun = scoringutils::log_shift,
-      offset = log_shift_offset
-    )
+    disease = disease
+  )
 
-  scored <- to_score |>
-    scoringutils::score(
-      metrics = c(
-        scoringutils::get_metrics(to_score),
-        list(interval_coverage_95 = interval_coverage_95)
-      )
-    ) |>
-    dplyr::filter(scale == "log")
-
-  return(scored)
+  return(path_df)
 }
 
 
-last_target_date <- lubridate::ymd("2024-11-30")
+score_and_save <- function(truth_data_path,
+                           influenza_table_dir,
+                           covid_table_dir,
+                           output_dir,
+                           last_target_date = NULL,
+                           horizons = c(0, 1)) {
+  scoring_date <- lubridate::today()
 
-truth_path <- "~/epiweekly.tsv"
-all_paths_flu <- c(
-  "~/2024-11-04-influenza-hubverse-table.tsv",
-  "~/2024-11-13-influenza-hubverse-table.tsv",
-  "~/2024-11-20-influenza-hubverse-table.tsv",
-  "~/2024-11-27-influenza-hubverse-table.tsv"
-)
-all_paths_covid <- c(
-  "~/2024-11-04-covid-hubverse-table.tsv",
-  "~/2024-11-13-covid-19-hubverse-table.tsv",
-  "~/2024-11-20-covid-19-hubverse-table.tsv",
-  "~/2024-11-27-covid-19-hubverse-table.tsv"
-)
+  all_paths <- dplyr::bind_rows(
+    get_hubverse_table_paths(
+      influenza_table_dir,
+      "influenza"
+    ),
+    get_hubverse_table_paths(
+      covid_table_dir,
+      "covid-19"
+    )
+  )
 
-flu_scores <- purrr::map(
-  all_paths_flu,
-  \(x) score_hubverse(truth_path, x, prop_influenza)
-) |>
-  dplyr::bind_rows() |>
-  dplyr::filter(target_end_date <= !!last_target_date)
-covid_scores <- purrr::map(
-  all_paths_covid,
-  \(x) score_hubverse(truth_path, x, prop_covid)
-) |>
-  dplyr::bind_rows() |>
-  dplyr::filter(target_end_date <= !!last_target_date)
+  truth_data <- readr::read_tsv(
+    truth_data_path,
+    show_col_types = FALSE
+  )
 
-full_scores <- dplyr::bind_rows(
-  flu_scores,
-  covid_scores
-)
-
-
-summary_by_epiweek <- full_scores |>
-  scoringutils::summarise_scores(by = c("horizon", "reference_date", "target"))
-
-summary_overall <- full_scores |>
-  scoringutils::summarise_scores(by = c("horizon", "target"))
-
-
-summary_by_loc <- full_scores |>
-  dplyr::filter(horizon %in% c(0, 1)) |>
-  scoringutils::summarise_scores(by = c("horizon", "location", "target"))
-
-
-coverage_figs <- purrr::map(
-  c(0.5, 0.95),
-  \(x) {
-    forecasttools::plot_coverage_by_date(
-      full_scores, x
-    ) +
-      ggplot2::theme_minimal()
+  if (is.null(last_target_date)) {
+    ## if no last target date, use a placeholder
+    last_target_date <- lubridate::ymd("9999-01-01")
   }
-)
+
+  read_and_score <- function(path, disease) {
+    scored <- readr::read_tsv(
+      x,
+      show_col_types = FALSE
+    ) |>
+      dplyr::mutate(disease = !!disease) |>
+      dplyr::filter(.data$target_end_date <= !!last_target_date) |>
+      hewr::score_hubverse(
+        observed_value_column = "prop_{disease}",
+        horizons = horizons
+      )
+
+    return(scored)
+  }
+
+  full_scores <- all_paths |>
+    purrr::pmap(read_and_score) |>
+    dplyr::bind_rows()
+
+  desired_summaries <- list(
+    summary_overall = c("horizon", "target"),
+    summary_by_epiweek = c(
+      "horizon",
+      "reference_date",
+      "target"
+    ),
+    summary_by_location = c(
+      "horizon",
+      "location",
+      "target"
+    )
+  )
+
+  summaries <- desired_summaries |>
+    purrr::map(\(x) scoringutils::summarise_scores(full_scores, by = x))
 
 
-forecasttools::plots_to_pdf(
-  coverage_figs,
-  "2024-11-29-coverage.pdf",
-  width = 11,
-  height = 8.5
-)
+  coverage_figs <- purrr::map(
+    c(0.5, 0.95),
+    \(x) {
+      forecasttools::plot_coverage_by_date(
+        full_scores, x
+      ) +
+        ggplot2::theme_minimal()
+    }
+  )
 
-readr::write_tsv(full_summary, "2024-11-30-scoring-summary.tsv")
+  make_output_path <- function(output_name,
+                               extension) {
+    return(fs::path(output_dir,
+      glue::glue("{scoring_date}-{output_name}"),
+      ext = extension
+    ))
+  }
+
+  forecasttools::plots_to_pdf(
+    coverage_figs,
+    make_output_path(
+      "coverage",
+      "pdf"
+    ),
+    width = 11,
+    height = 8.5
+  )
+
+  purrr::walk2(
+    summaries,
+    names(summaries),
+    \(x, y) {
+      readr::write_tsv(
+        x,
+        make_output_path(
+          y,
+          "tsv"
+        )
+      )
+    }
+  )
+}
