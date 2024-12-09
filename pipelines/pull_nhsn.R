ef031b1a342e36b026de22e5cc27a59b9097495c
Damon Bayer <xum8@cdc.gov>
2024-12-09 22:38:18 +0000

    pre-commit

--- pipelines/pull_nhsn.R
+++ pipelines/pull_nhsn.R
@@ -1,46 +1,46 @@
 script_packages <- c(
-    "forecasttools",
-    "tidyverse",
-    "argparser"
+  "forecasttools",
+  "tidyverse",
+  "argparser"
 )
 
 # load in packages without messages
 purrr::walk(script_packages, \(pkg) {
-    suppressPackageStartupMessages(
-        library(pkg, character.only = TRUE)
-    )
+  suppressPackageStartupMessages(
+    library(pkg, character.only = TRUE)
+  )
 })
 
 disease_nhsn_key <- c(
-    "COVID-19" = "totalconfc19newadm",
-    "Influenza" = "totalconfflunewadm"
+  "COVID-19" = "totalconfc19newadm",
+  "Influenza" = "totalconfflunewadm"
 )
 
 p <- arg_parser(
-    "Pull NHSN data"
+  "Pull NHSN data"
 ) |>
-    add_argument(
-        "--start-date",
-        type = "character",
-        default = NULL,
-        help = "Start date in YYYY-MM-DD format"
-    ) |>
-    add_argument(
-        "--end-date",
-        type = "character",
-        default = NULL,
-        help = "End date in YYYY-MM-DD format"
-    ) |>
-    add_argument(
-        "--disease",
-        type = "character",
-        help = "Disease name"
-    ) |>
-    add_argument(
-        "--output-file",
-        type = "character",
-        help = "Path to output file"
-    )
+  add_argument(
+    "--start-date",
+    type = "character",
+    default = NULL,
+    help = "Start date in YYYY-MM-DD format"
+  ) |>
+  add_argument(
+    "--end-date",
+    type = "character",
+    default = NULL,
+    help = "End date in YYYY-MM-DD format"
+  ) |>
+  add_argument(
+    "--disease",
+    type = "character",
+    help = "Disease name"
+  ) |>
+  add_argument(
+    "--output-file",
+    type = "character",
+    help = "Path to output file"
+  )
 
 
 argv <- parse_args(p)
@@ -50,17 +50,17 @@ disease <- argv$disease
 output_file <- argv$output_file
 
 if (is.na(output_file)) {
-    output_file <- stdout()
+  output_file <- stdout()
 }
 
 columns <- disease_nhsn_key[disease]
 
 dat <- pull_nhsn(
-    start_date = start_date,
-    end_date = end_date,
-    columns = columns
+  start_date = start_date,
+  end_date = end_date,
+  columns = columns
 ) |>
-    mutate(weekendingdate = as_date(weekendingdate)) |>
-    rename(nhsn_admissions = !!unname(columns)) |>
-    mutate(nhsn_admissions = as.numeric(nhsn_admissions)) |>
-    write_tsv(output_file)
+  mutate(weekendingdate = as_date(weekendingdate)) |>
+  rename(nhsn_admissions = !!unname(columns)) |>
+  mutate(nhsn_admissions = as.numeric(nhsn_admissions)) |>
+  write_tsv(output_file)
