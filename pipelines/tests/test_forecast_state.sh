682959112710ce479887e479187008bdf82ddbbe
Samuel Brand <sam055@mac.com>
2024-12-17 02:43:33 +0000

    Create test_forecast_state.sh

---
+++ pipelines/tests/test_forecast_state.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if the base directory is provided as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <base_dir>"
+    exit 1
+fi
+
+BASE_DIR="$1"
+
+Rscript generate_test_data.R "$BASE_DIR/private_data"
+python forecast_state.py \
+    --disease "COVID-19" \
+    --state "CA" \
+    --facility-level-nssp-data-dir "$BASE_DIR/private_data/nssp_etl_gold" \
+    --state-level-nssp-data-dir "$BASE_DIR/private_data/nssp_state_level_gold" \
+    --priors-path "$BASE_DIR/test_output/priors.py" \
+    --param-data-dir "$BASE_DIR/private_data/prod_param_estimates" \
+    --output-dir "BASE_DIR/private_data" \
+    --n-training-days 90 \
+    --n-chains 1 \
+    --score \
+    --eval-data-path "$BASE_DIR/test_output"
