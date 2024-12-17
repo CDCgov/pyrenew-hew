a2bea5458d638340eafa96cfdaee921e300132d5
Samuel Brand <sam055@mac.com>
2024-12-17 14:16:32 +0000

    add explicit fails to pipeline run
    These will trigger CI fails to avoid false CI passes

--- pipelines/tests/test_forecast_state.sh
+++ pipelines/tests/test_forecast_state.sh
@@ -7,8 +7,15 @@ if [ -z "$1" ]; then
 fi
 
 BASE_DIR="$1"
-
+echo "TEST-MODE: Running forecast_state.py in test mode with base directory $BASE_DIR"
 Rscript pipelines/generate_test_data.R "$BASE_DIR/private_data"
+if [ $? -ne 0 ]; then
+    echo "TEST-MODE FAIL: Generating test data failed"
+    exit 1
+else
+    echo "TEST-MODE: Finished generating test data"
+fi
+echo "TEST-MODE: Running forecasting pipeline"
 python pipelines/forecast_state.py \
     --disease "COVID-19" \
     --state "CA" \
@@ -23,3 +30,10 @@ python pipelines/forecast_state.py \
     --n-warmup 500 \
     --score \
     --eval-data-path "$BASE_DIR/private_data/nssp-archival-vintages"
+if [ $? -ne 0 ]; then
+    echo "TEST-MODE FAIL: Forecasting/postprocessing/scoring pipeline failed"
+    exit 1
+else
+    echo "TEST-MODE: Finished forecasting/postprocessing/scoring pipeline"
+fi
+echo "TEST-MODE: All finished successfully"
