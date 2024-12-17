d0eaff09c0e55dd1a92f357a7d3f1a49cf5235dd
Samuel Brand <sam055@mac.com>
2024-12-17 13:09:15 +0000

    reduce number of samples

--- pipelines/tests/test_forecast_state.sh
+++ pipelines/tests/test_forecast_state.sh
@@ -8,8 +8,8 @@ fi
 
 BASE_DIR="$1"
 
-Rscript generate_test_data.R "$BASE_DIR/private_data"
-python forecast_state.py \
+Rscript pipelines/generate_test_data.R "$BASE_DIR/private_data"
+python pipelines/forecast_state.py \
     --disease "COVID-19" \
     --state "CA" \
     --facility-level-nssp-data-dir "$BASE_DIR/private_data/nssp_etl_gold" \
@@ -19,5 +19,7 @@ python forecast_state.py \
     --output-dir "$BASE_DIR/private_data" \
     --n-training-days 90 \
     --n-chains 1 \
+    --n-samples 500 \
+    --n-warmup 500 \
     --score \
     --eval-data-path "$BASE_DIR/private_data/nssp-archival-vintages"
