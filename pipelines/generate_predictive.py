d5c0199709ef84052970e03f3b5b9bad3721cbe1
Damon Bayer <xum8@cdc.gov>
2024-12-30 11:56:58 -0600

    Update pyrenew_hew_model sample signature for PyRenew-HE (#265)
    * Create README.md
    
    * move/rename build_model.py
    
    * delete unused files
    
    * move and rename
    
    * change import references
    
    * pre-commit
    
    * adjust imports
    
    * pre-commit
    
    * remove extra forecast_state
    
    * undo lots of changes
    
    * restore readme
    
    * remove more inits
    
    * remove another init
    
    * correct import
    
    * pre-commit
    
    * rename n_datapoints
    
    * recommit n_datapoints change
    
    * allow observing hospital admissions
    
    * update predictive and fix names
    
    ---------
    
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>

--- pipelines/generate_predictive.py
+++ pipelines/generate_predictive.py
@@ -18,6 +18,7 @@ def generate_and_save_predictions(
     (
         my_model,
         data_observed_disease_ed_visits,
+        data_observed_disease_hospital_admissions,
         right_truncation_offset,
     ) = build_model_from_dir(model_run_dir)
 
@@ -33,7 +34,14 @@ def generate_and_save_predictions(
     my_model.mcmc.sampler = fresh_sampler
 
     posterior_predictive = my_model.posterior_predictive(
-        n_datapoints=len(data_observed_disease_ed_visits) + n_forecast_points
+        n_observed_disease_ed_visits_datapoints=len(
+            data_observed_disease_ed_visits
+        )
+        + n_forecast_points,
+        n_observed_hospital_admissions_datapoints=len(
+            data_observed_disease_hospital_admissions
+        )
+        + n_forecast_points // 7,
     )
 
     idata = az.from_numpyro(
