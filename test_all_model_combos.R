da9c968ba0d4598709c6c303b85b2ca695f4eb8d
Damon Bayer <damon@damonbayer.com>
2025-01-13 17:55:30 -0600

    revise all model combos test

--- test_all_model_combos.R
+++ test_all_model_combos.R
@@ -1,26 +1,37 @@
 library(tidyverse)
 library(fs)
-model_batch_dir <- path("/Users/damon/Downloads/Archive/covid-19_r_2024-12-21_f_2024-10-22_t_2024-12-20/model_runs/")
-model_run_dir <- path(model_batch_dir, "CA")
-
+library(hewr)
 source("~/Documents/GitHub/pyrenew-hew/hewr/R/process_state_forecast.R")
+model_batch_dirs <- c(
+  path(
+    "/Users/damon/Documents/GitHub/pyrenew-hew/pipelines/tests/private_data",
+    "covid-19_r_2024-12-21_f_2024-10-22_t_2024-12-20"
+  ),
+  path(
+    "/Users/damon/Documents/GitHub/pyrenew-hew/pipelines/tests/private_data",
+    "influenza_r_2024-12-21_f_2024-10-22_t_2024-12-20"
+  )
+)
 
-dir_copy(path(model_run_dir, "pyrenew_e"), path(model_run_dir, "pyrenew_h"))
+model_run_dirs <- dir_ls(path(model_batch_dirs, "model_runs"))
 
-pyrenew_model_name <- "pyrenew_h"
-timeseries_model_name <- "NULL"
-process_state_forecast(model_run_dir = model_run_dir,
-                       pyrenew_model_name = "pyrenew_h",
-                       timeseries_model_name = NULL)
+walk(model_run_dirs, \(model_run_dir) {
+  process_state_forecast(
+    model_run_dir = model_run_dir,
+    pyrenew_model_name = "pyrenew_h",
+    timeseries_model_name = NULL
+  )
 
-pyrenew_model_name <- "pyrenew_he"
-timeseries_model_name <- "timeseries_e"
-process_state_forecast(model_run_dir = model_run_dir,
-                       pyrenew_model_name = "pyrenew_e",
-                       timeseries_model_name = "timeseries_e")
 
-pyrenew_model_name <- "pyrenew_e"
-timeseries_model_name <- "timeseries_e"
-process_state_forecast(model_run_dir = model_run_dir, |>
-                         pyrenew_model_name = "pyrenew_he",
-                       timeseries_model_name = "timeseries_e")
+  process_state_forecast(
+    model_run_dir = model_run_dir,
+    pyrenew_model_name = "pyrenew_e",
+    timeseries_model_name = "timeseries_e"
+  )
+
+  process_state_forecast(
+    model_run_dir = model_run_dir,
+    pyrenew_model_name = "pyrenew_he",
+    timeseries_model_name = "timeseries_e"
+  )
+})
