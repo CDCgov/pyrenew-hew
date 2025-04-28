a0626b41cdef909d23722f92a5a35a5abf4df9fd
sbidari <zib2@cdc.gov>
2025-04-28 17:59:13 -0400

    update test

--- tests/test_hospital_admission_indices.py
+++ tests/test_hospital_admission_indices.py
@@ -1,3 +1,5 @@
+import itertools
+
 import jax.numpy as jnp
 import pytest
 
@@ -8,27 +10,29 @@ from pyrenew_hew.pyrenew_hew_model import HospAdmitObservationProcess
     [
         "first_latent_admission_dow",
         "model_t_first_latent_admissions",
-        "n_datapoints",
         "model_t_observed",
-        "expected_result",
     ],
     [
-        [2, 0, 7, None, jnp.arange(7)],
-        [2, 0, None, jnp.array([10, 17, 24]), jnp.array([0, 1, 2])],
-        [2, 0, 5, jnp.array([10, 17, 24]), jnp.array([0, 1, 2])],
+        [0, -2, jnp.array([10, 17, 24])],
+        [1, -4, jnp.array([7, 14, 21])],
+        [4, -2, jnp.array([6, 13, 20])],
+        [3, 1, jnp.array([17, 24, 31])],
     ],
 )
 def test_calculate_weekly_hosp_indices(
     first_latent_admission_dow,
     model_t_first_latent_admissions,
-    n_datapoints,
     model_t_observed,
-    expected_result,
 ):
     result = HospAdmitObservationProcess.calculate_weekly_hosp_indices(
         first_latent_admission_dow,
         model_t_first_latent_admissions,
-        n_datapoints,
         model_t_observed,
     )
+    expected_result = (
+        model_t_observed
+        - model_t_first_latent_admissions
+        - (6 - first_latent_admission_dow) % 7
+        - 6
+    ) // 7
     assert jnp.array_equal(result, expected_result)
