219f6b90aac183acb32b77ac4e18558cb4e2492e
Subekshya <zib2@cdc.gov>
2025-05-13 21:32:43 +0000

    normalize ww subpop

--- pipelines/prep_data.py
+++ pipelines/prep_data.py
@@ -552,7 +552,7 @@ def process_and_save_state(
                 / state_pop
             )
         else:
-            pop_fraction = subpop_sizes / state_pop
+            pop_fraction = subpop_sizes / sum(subpop_sizes)
 
     data_for_model_fit = {
         "inf_to_hosp_admit_pmf": delay_pmf,
