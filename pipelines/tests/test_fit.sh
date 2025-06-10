a27a585d2e03fee08be01e498ceb14056e099be8
Damon Bayer <damon@damonbayer.com>
2025-06-10 12:34:09 -0500

    90 day test fit

--- pipelines/tests/test_fit.sh
+++ pipelines/tests/test_fit.sh
@@ -20,7 +20,7 @@ python pipelines/forecast_loc.py \
 	--param-data-dir "$BASE_DIR/private_data/prod_param_estimates" \
 	--nwss-data-dir "$BASE_DIR/private_data/nwss_vintages" \
 	--output-dir "$BASE_DIR/2024-12-21_forecasts" \
-	--n-training-days 60 \
+	--n-training-days 90 \
 	--n-chains 2 \
 	--n-samples 250 \
 	--n-warmup 250 \
