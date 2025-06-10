757f0b11af58273f3fec8d031022249d240418f6
Damon Bayer <damon@damonbayer.com>
2025-06-10 13:29:27 -0500

    fix nhsn eval data

--- pipelines/forecast_loc.py
+++ pipelines/forecast_loc.py
@@ -405,6 +405,7 @@ def main(
         output_data_dir=Path(model_run_dir, "data"),
         last_eval_date=report_date + timedelta(days=n_forecast_days),
         credentials_dict=credentials_dict,
+        nhsn_data_path=nhsn_data_path,
     )
     logger.info("Done getting eval data.")
 
