729660558c31e2cbe64ef26cf2cdfab6c163be76
Damon Bayer <xum8@cdc.gov>
2025-07-11 16:35:17 -0500

    Create common list of locations and diseases for end to end tests (#579)

--- pipelines/tests/test_pyrenew_fit.sh
+++ pipelines/tests/test_pyrenew_fit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if the base directory is provided as an argument
-if [[ $# -lt 4 ]]; then
+if [[ $# -ne 4 ]]; then
 	echo "Usage: $0 <base_dir> <disease> <location> <model_letters>"
 	exit 1
 fi
@@ -12,8 +12,8 @@ location="$3"
 model_letters="$4"
 
 python pipelines/forecast_pyrenew.py \
-	--disease $disease \
-	--loc $location \
+	--disease "$disease" \
+	--loc "$location" \
 	--facility-level-nssp-data-dir "$BASE_DIR/private_data/nssp_etl_gold" \
 	--state-level-nssp-data-dir "$BASE_DIR/private_data/nssp_state_level_gold" \
 	--priors-path pipelines/priors/prod_priors.py \
@@ -24,11 +24,11 @@ python pipelines/forecast_pyrenew.py \
 	--n-chains 2 \
 	--n-samples 250 \
 	--n-warmup 250 \
-	--model-letters $model_letters \
-	--additional-forecast-letters $model_letters \
+	--model-letters "$model_letters" \
+	--additional-forecast-letters "$model_letters" \
 	--eval-data-path "$BASE_DIR/private_data/nssp-etl" \
 	--nhsn-data-path "$BASE_DIR/private_data/nhsn_test_data/${disease}_${location}.parquet"
-if [ $? -ne 0 ]; then
+if [ "$?" -ne 0 ]; then
 	echo "TEST-MODE FAIL: Forecasting/postprocessing pipeline failed"
 	exit 1
 fi
