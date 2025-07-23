6ae281630ebc8ddacfa354de77a2f6c93f0a3bde
jkislin <qxk3@cdc.gov>
2025-07-23 21:21:00 +0000

    having my cake and eating it too with optional boolean flags for --test and --dry-run

--- pipelines/batch/setup_job.py
+++ pipelines/batch/setup_job.py
@@ -394,17 +394,35 @@ if __name__ == "__main__":
         ),
         default="pyrenew",
     )
+    # Function to convert string to boolean
+    # This is used to allow passing boolean values as command line arguments
+    # Reference: https://docs.python.org/3/library/argparse.html#type
+    # and
+    # https://stackoverflow.com/a/43357954
+    def str2bool(v):
+        if isinstance(v, bool):
+            return v
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')
+
+    # With the str2bool type, you can supply the flag for True, omit it for False, or pass True/False explicitly
     parser.add_argument(
         "--test",
-        type=bool,
+        type=str2bool,
         nargs="?",
         const=True,
         default=False,
         help="Run in test mode (default: False). Pass --test True or --test False to set explicitly.",
     )
+
+    # With the str2bool type, you can supply the flag for True, omit it for False, or pass True/False explicitly
     parser.add_argument(
-        "--dry_run",
-        type=bool,
+        "--dry-run",
+        type=str2bool,
         nargs="?",
         const=True,
         default=False,
