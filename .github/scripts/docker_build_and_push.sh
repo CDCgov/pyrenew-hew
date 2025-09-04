9a88b93bd8ed0d4bb5ee248d47956ec7bafff6dd
Jon Kislin <136386616+jkislin@users.noreply.github.com>
2025-09-04 20:11:41 +0000

    commented out cache code in docker_build_and_push.sh

--- .github/scripts/docker_build_and_push.sh
+++ .github/scripts/docker_build_and_push.sh
@@ -8,11 +8,19 @@ BUILDER=docker-container-driver-builder
 docker buildx create --name "$BUILDER" --driver=docker-container || true
 
 # use the registry cache for prior images of the same tag, or the 'latest' tag
+# time docker buildx build --push -t "$IMAGE:$TAG" \
+# 	--builder "$BUILDER" \
+# 	--build-arg "GIT_COMMIT_SHA=$GIT_COMMIT_SHA" \
+# 	--build-arg "GIT_BRANCH_NAME=$GIT_BRANCH_NAME" \
+# 	--cache-from "type=registry,ref=$IMAGE:$TAG-cache" \
+# 	--cache-from "type=registry,ref=$IMAGE:latest-cache" \
+# 	--cache-to "type=registry,ref=$IMAGE:$TAG-cache,mode=max" \
+# 	-f Containerfile .
+
+# attempt without registry cache - we care less about speed 
+# and more about deterministic rebuilds
 time docker buildx build --push -t "$IMAGE:$TAG" \
 	--builder "$BUILDER" \
 	--build-arg "GIT_COMMIT_SHA=$GIT_COMMIT_SHA" \
 	--build-arg "GIT_BRANCH_NAME=$GIT_BRANCH_NAME" \
-	--cache-from "type=registry,ref=$IMAGE:$TAG-cache" \
-	--cache-from "type=registry,ref=$IMAGE:latest-cache" \
-	--cache-to "type=registry,ref=$IMAGE:$TAG-cache,mode=max" \
 	-f Containerfile .
