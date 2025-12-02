d615505651f21e5f2913b52edfb0f7a20ba69e29
jkislin <qxk3@cdc.gov>
2025-12-02 21:05:14 +0000

    adding env variables for Azure Command Center

--- blobfuse/mount.sh
+++ blobfuse/mount.sh
@@ -25,6 +25,7 @@ done
 
 echo ""
 echo "Creating symlinks in $(pwd)..."
+
 ln -s "/mnt/prod-param-estimates" "./params"
 ln -s "/mnt/pyrenew-hew-prod-output" "./output"
 ln -s "/mnt/pyrenew-test-output" "./test-output"
@@ -32,5 +33,10 @@ ln -s "/mnt/nwss-vintages" "./nwss-vintages"
 ln -s "/mnt/pyrenew-hew-config" "./config"
 ln -s "/mnt/nssp-etl" "./nssp-etl"
 ln -s "/mnt/nssp-archival-vintages" "./nssp-archival-vintages"
+
+echo "Setting environment variables for the Azure Command Center..."
+export NSSP_ETL_PATH="$(pwd)/nssp-etl"
+export PYRENEW_HEW_PROD_OUTPUT_PATH="$(pwd)/output"
+export NWSS_VINTAGES_PATH="$(pwd)/nwss-vintages"
 
 echo "Done."
