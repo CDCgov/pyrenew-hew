03fbb7446fbdc8f9f7b1bba666b554ac63fec084
Damon Bayer <damon@damonbayer.com>
2026-01-30 14:57:25 -0600

    fix ts gha

--- pipelines/tests/test_ts_fit.sh
+++ pipelines/tests/test_ts_fit.sh
@@ -9,7 +9,6 @@ fi
 BASE_DIR="$1"
 disease="$2"
 location="$3"
-model_letters="$4"
 
 python pipelines/forecast_timeseries.py \
 	--disease "$disease" \
@@ -19,7 +18,6 @@ python pipelines/forecast_timeseries.py \
 	--output-dir "$BASE_DIR/2024-12-21_forecasts" \
 	--n-training-days 90 \
 	--n-samples 500 \
-	--model-letters "$model_letters" \
 	--nhsn-data-path "$BASE_DIR/private_data/nhsn_test_data/${disease}_${location}.parquet"
 if [ "$?" -ne 0 ]; then
 	echo "TEST-MODE FAIL: Forecasting/postprocessing pipeline failed"
