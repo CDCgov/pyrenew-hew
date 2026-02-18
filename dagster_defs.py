eabc352d624b380643b53105aa8e4de57e47a427
jkislin <qxk3@cdc.gov>
2026-02-18 00:01:06 +0000

    gave the container app job launcher for our backfill job more resources

--- dagster_defs.py
+++ dagster_defs.py
@@ -602,13 +602,28 @@ def launch_pyrenew_pipeline(
 
 ## -- Jobs -- ##
 
+# These specify the resources used to initially launch our backfill job.
+pyrenew_pipeline_caj_launch_config = {
+    "config": {
+        "launcher": {"AzureContainerAppJobRunLauncher": {"cpu": 2.0, "memory": 4.0}}
+    }
+}
+pyrenew_pipeline_local_launch_config = {
+    "config": {}
+}  # We can let the default take over
+
 
 # This wraps our launch_pipeline op in a job that can be scheduled or manually launched via the GUI
 @dg.job(
     executor_def=dynamic_executor(
         default_config=azure_caj_config if is_production else default_config
     ),
-    config=dg.RunConfig(ops={"launch_pyrenew_pipeline": ModelConfig()}),
+    config=dg.RunConfig(
+        ops={"launch_pyrenew_pipeline": ModelConfig()},
+        execution=pyrenew_pipeline_caj_launch_config
+        if is_production
+        else pyrenew_pipeline_local_launch_config,
+    ),
 )
 def weekly_pyrenew_via_backfill():
     launch_pyrenew_pipeline()
