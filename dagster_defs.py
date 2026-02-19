e4844b3bc139b9ac03020ce5941581fdbcb3fa9e
jkislin <qxk3@cdc.gov>
2026-02-19 18:14:12 +0000

    removing automatic run of postprocess_forecasts for now; will followup in next pr, more config needed

--- dagster_defs.py
+++ dagster_defs.py
@@ -538,13 +538,7 @@ def launch_pyrenew_pipeline(
         context.log.info(
             "Launching a timeseries_e, pyrenew_e, pyrenew_h, and pyrenew_he backfill."
         )
-        asset_selection = [
-            "timeseries_e",
-            "pyrenew_e",
-            "pyrenew_h",
-            "pyrenew_he",
-            "postprocess_forecasts",
-        ]
+        asset_selection = ["timeseries_e", "pyrenew_e", "pyrenew_h", "pyrenew_he"]
 
     # elif nhsn_available and nwss_available:
     #     context.log.info("NHSN data and NWSS data are available, but NSSP gold data is not.")
@@ -554,12 +548,12 @@ def launch_pyrenew_pipeline(
     elif nssp_available:
         context.log.info("Only NSSP gold data are available.")
         context.log.info("Launching a timeseries_e and pyrenew_e backfill.")
-        asset_selection = ["timeseries_e", "pyrenew_e", "postprocess_forecasts"]
+        asset_selection = ["timeseries_e", "pyrenew_e"]
 
     elif nhsn_available:
         context.log.info("Only NHSN data are available.")
         context.log.info("Launching a pyrenew_h backfill.")
-        asset_selection = ["pyrenew_h", "postprocess_forecasts"]
+        asset_selection = ["pyrenew_h"]
 
     else:
         context.log.info("No required data is available.")
